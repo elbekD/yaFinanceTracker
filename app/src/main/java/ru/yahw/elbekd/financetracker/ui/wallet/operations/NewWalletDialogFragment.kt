55c619a19000179445c653b60ef78cff4de4c1b2
Elbek Djuraev <djuraev.elbek@gmail.com>
2018-07-30 16:27:07 +0300

    remove third party dialog lib

--- app/src/main/java/ru/yahw/elbekd/financetracker/ui/wallet/operations/NewWalletDialogFragment.kt
+++ app/src/main/java/ru/yahw/elbekd/financetracker/ui/wallet/operations/NewWalletDialogFragment.kt
@@ -1,8 +1,8 @@
 package ru.yahw.elbekd.financetracker.ui.wallet.operations
 
+import android.app.AlertDialog
 import android.app.Dialog
 import android.os.Bundle
-import com.afollestad.materialdialogs.MaterialDialog
 import ru.yahw.elbekd.financetracker.di.Injectable
 import ru.yahw.elbekd.financetracker.ui.base.BaseDialog
 import ru.yahw.elbekd.financetracker.ui.wallet.WalletViewModel
@@ -12,7 +12,7 @@ import ru.yahw.elbekd.financetracker.ui.wallet.WalletViewModel
  */
 class NewWalletDialogFragment : BaseDialog<WalletViewModel>(), Injectable {
     private lateinit var vm: WalletViewModel
-    private lateinit var dialog: MaterialDialog
+    private lateinit var dialog: AlertDialog
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         vm = getViewModel()
         return dialog
